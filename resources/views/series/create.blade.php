256d96165eabae1d9958e6ef56bbc52e4ce081d1
Thiago Marçal <thiagomarcal1984@gmail.com>
2022-10-02 15:47:06 -0300

    Estilizando as views.

--- resources/views/series/create.blade.php
+++ resources/views/series/create.blade.php
@@ -1,6 +1,9 @@
 <x-layout title="Nova Série">
     <form action="" method="post">
-        <label for="nome">Nome: </label>
-        <input type="text" id="nome" name="nome">
+        <div class="mb-3">
+            <label class="form-label" for="nome">Nome: </label>
+            <input class="form-control" type="text" id="nome" name="nome">
+        </div>
+        <button type="submit" class="btn btn-primary">Adicionar</button>
     </form>
 </x-layout>
--- resources/views/series/index.blade.php
+++ resources/views/series/index.blade.php
@@ -1,8 +1,8 @@
 <x-layout title="Lista de Séries">
-    <a href="/series/criar">Adicionar</a>
-    <ul>
+    <a class="btn btn-dark mb-2" href="/series/criar">Adicionar</a>
+    <ul class="list-group">
         @foreach($series as $serie)
-        <li>{{ $serie }}</li>
+        <li class="list-group-item">{{ $serie }}</li>
         @endforeach
     </ul>
 </x-layout>
